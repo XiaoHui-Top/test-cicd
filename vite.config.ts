9c15698103354a50ced6f58bd4cf08c6064df741
XIAOHUI <xiaohui8561@qq.com>
2025-06-06 14:11:02 +0800

    fix: 添加根路径

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base:'./',// 将根路径换成相对路径
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
